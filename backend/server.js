1344de0a8aa1dbacc0a54a2c6acd276db79b1d7c
bekalu73 <bekalusisay2010@gmail.com>
2024-06-11 11:17:04 +0300

    create a book model with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 import express from "express";
-import { PORT } from "./config.js";
+// import { PORT, MONGODB_URI } from "./config.js";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
 const app = express();
+
+const DBConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("database Connected");
+  } catch (err) {
+    console.log("database Connection Error", err);
+  }
+};
 
 app.get("/", (req, res) => {
   console.log("this is home page");
   res.send("home");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is Listening on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is Listening on http://localhost:${process.env.PORT}`);
+  DBConnection();
 });
